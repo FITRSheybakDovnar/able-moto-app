3703616273d7fe1e4f5d1b1b5bf4cb1a19bd9bbc
SheybakDmitry <37551955+SheybakDmitry@users.noreply.github.com>
2018-03-20 12:57:07 +0300

    Update hello_world.java

--- hello_world.java
+++ hello_world.java
@@ -2,7 +2,7 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
-        System.out.println("уууууееее");
+        System.out.println("ууууепороуееее");
     }
 
 }
